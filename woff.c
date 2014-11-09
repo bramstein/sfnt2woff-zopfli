da4d5599ddf176b08137e0866fb2ab799fbeb2f7
Bram Stein <stein@adobe.com>
2014-11-09 10:40:45 +0100

    Add myself as contributor to make Zopfli modifications available under the same terms as the original sfnt2woff code.

--- woff.c
+++ woff.c
@@ -20,6 +20,7 @@
  *
  * Contributor(s):
  *   Jonathan Kew <jfkthame@gmail.com>
+ *   Bram Stein <b.l.stein@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
